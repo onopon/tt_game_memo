7f13044b3e3a3c2e0afeb3ba970020c032cc28f7
onopon <kiyopon.onopon@gmail.com>
2022-04-25 10:10:02 +0900

    add memo display
    Signed-off-by: onopon <kiyopon.onopon@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,37 +1,45 @@
+MAIN_PLAYER_ID = 2980
+
 function loadAllPlayers() {
-    var playerList = new Array();
-    getPlayers().forEach(player => {
-        var dataList = player.split(',');
-        var playerData = {playerID: dataList[0],
-            name: dataList[1],
-            country: dataList[2],
-            region: dataList[3],
-            klass: dataList[4]};
-        playerList.push(playerData);
-    });
-    return playerList;
+  var playerList = new Array();
+  getPlayers().forEach(player => {
+    var dataList = player.split(',');
+    var playerData = {playerID: dataList[0],
+      name: dataList[1],
+      country: dataList[2],
+      region: dataList[3],
+      klass: dataList[4]};
+    playerList.push(playerData);
+  });
+  return playerList;
 }
 
 function getLastScoreValues(scoreEl) {
-    var rows = scoreEl.getElementsByClassName('row');
-    var lastScoreValues = [];
-    for(i = 0; i < rows.length; i++) {
-        var row = rows[i];
-        var scores = row.getElementsByClassName('score')[0].getElementsByTagName('input');
-        var reverseScores = Array.from(scores);
-        reverseScores.reverse();
-        var lastScore = reverseScores.find(val => val.value !== '');
-        var lastScoreVal = Number(lastScore ? lastScore.value : 0);
-        lastScoreValues.push(lastScoreVal);
-    }
-    return lastScoreValues;
+  var rows = scoreEl.getElementsByClassName('score-flow')[0].getElementsByClassName('row');
+  var lastScoreValues = [];
+  for(i = 0; i < rows.length; i++) {
+    var row = rows[i];
+    var scores = row.getElementsByClassName('score')[0].getElementsByTagName('input');
+    var reverseScores = Array.from(scores);
+    reverseScores.reverse();
+    var lastScore = reverseScores.find(val => val.value !== '');
+    var lastScoreVal = Number(lastScore ? lastScore.value : 0);
+    lastScoreValues.push(lastScoreVal);
+  }
+  return lastScoreValues;
 }
 
 function isGameFinish(scoreEl) {
-    scoreValues = getLastScoreValues(scoreEl);
-    if ((scoreValues[0] >= 11 || scoreValues[1] >= 11) && Math.abs(scoreValues[0] - scoreValues[1]) >= 2) return true;
-    if ((scoreValues[0] >= 10 && scoreValues[1] >= 10) && Math.abs(scoreValues[0] - scoreValues[1]) >= 2) return true;
-    return false;
+  scoreValues = getLastScoreValues(scoreEl);
+  if ((scoreValues[0] >= 11 || scoreValues[1] >= 11) && Math.abs(scoreValues[0] - scoreValues[1]) >= 2) return true;
+  if ((scoreValues[0] >= 10 && scoreValues[1] >= 10) && Math.abs(scoreValues[0] - scoreValues[1]) >= 2) return true;
+  return false;
+}
+
+function sumCount(scoreStr) { // ex) 8-9 -> return 17
+  var total = 0
+  scoreStr.split('-').forEach(s => { total += Number(s); });
+  return total;
 }
 
 var GameState = class {
@@ -77,123 +85,205 @@ var GameState = class {
 
 var Game = class {
   constructor() {
-      this.gameStates  = [new GameState(),
-                          new GameState(),
-                          new GameState(),
-                          new GameState(),
-                          new GameState()]
-      this.mFocusGameNumber = 1
+    this.gameStates  = [new GameState(),
+      new GameState(),
+      new GameState(),
+      new GameState(),
+      new GameState()]
+    this.mFocusGameNumber = 1
+    this.mTournamentName = ''
+    this.mTournament = 'FACTOR20'
+    this.mMainPlayerID = ''
+    this.mPlayerID = ''
+    this.mFirstBall = '1' // 1: Serve 2: Receive
+    var memoDefaults = []
+    for (var i = 1; i <= 5; i++) {
+      memoDefaults.push(`●第${i}ゲーム----------------------------`)
+    }
+    this.mMemo = memoDefaults.join('\n')
+    this.mPlayerMemo = memoDefaults.join('\n')
+    this.mPlayerPositiveAdvice = memoDefaults.join('\n')
+    this.mPlayerChanceAdvice = memoDefaults.join('\n')
+    this.mPlayerNextAdvice = memoDefaults.slice(0, 4).join('\n')
   }
 
-  get focusGameNumber() {
-    return this.mFocusGameNumber
-  }
-
-  set focusGameNumber(num) {
-    this.mFocusGameNumber = num
-  }
+  get focusGameNumber() { return this.mFocusGameNumber }
+  set focusGameNumber(num) { this.mFocusGameNumber = num }
+  get tournamentName() { return this.mTournamentName }
+  set tournamentName(val) { this.mTournamentName = val }
+  get tournament() { return this.mTournament }
+  set tournament(val) { this.mTournament = val }
+  get mainPlayerID() { return this.mMainPlayerID }
+  set mainPlayerID(val) { this.mMainPlayerID = val }
+  get playerID() { return this.mPlayerID }
+  set playerID(val) { this.mPlayerID = val }
+  get firstBall() { return this.mFirstBall }
+  set firstBall(val) {this.mFirstBall = val }
+  get memo() { return this.mMemo }
+  set memo(val) {this.mMemo = val }
+  get playerMemo() { return this.mPlayerMemo }
+  set playerMemo(val) {this.mPlayerMemo = val }
+  get playerPositiveAdvice() { return this.mPlayerPositiveAdvice }
+  set playerPositiveAdvice(val) {this.mPlayerPositiveAdvice = val }
+  get playerChanceAdvice() { return this.mPlayerChanceAdvice }
+  set playerChanceAdvice(val) {this.mPlayerChanceAdvice = val }
+  get playerNextAdvice() { return this.mPlayerNextAdvice }
+  set playerNextAdvice(val) {this.mPlayerNextAdvice = val }
 
   get gameState() {
     return this.gameStates[this.mFocusGameNumber - 1]
   }
-}
 
+  get positiveAdviceForDisplay() {
+    return this.playerPositiveAdvice.split('\n')[this.focusGameNumber*2 - 1]
+  }
+
+  get chanceAdviceForDisplay() {
+    return this.playerChanceAdvice.split('\n')[this.focusGameNumber*2 - 1]
+  }
+
+  get nextAdviceForDisplay() {
+    return this.playerNextAdvice.split('\n')[this.focusGameNumber*2 - 1]
+  }
+}
 var app = new Vue({
   el: '#app',
   data: {
     player: {},
-    playerID: "",
+    playerID: '',
+    mainPlayer: {},
     allPlayers: [],
-    date: "",
-    today: "",
-    game: new Game() 
+    date: '',
+    today: '',
+    game: new Game()
   },
   methods: {
     initialize() {
-        this.$set(this, 'allPlayers', loadAllPlayers());
-        var today = new Date();
-        this.$set(this, 'date', `${today.getFullYear()}/${today.getMonth()+1}/${today.getDate()}`);
-        this.changeFocus(1);
+      this.$set(this, 'allPlayers', loadAllPlayers());
+      var today = new Date();
+      this.$set(this, 'date', `${today.getFullYear()}/${today.getMonth()+1}/${today.getDate()}`);
+      this.$set(this, 'mainPlayer', this.allPlayers.find(p => p.playerID == MAIN_PLAYER_ID));
+      this.changeFocus(1);
+      for (var i = 1; i <= 5; i++) {
+        var scoreEl = document.getElementById(`scoreBoard${i}`);
+        this.addScoreDOM(scoreEl);
+//      this.addMemoDOM(scoreEl);
+      }
     },
     addPoint(isFirstPlayer = true) {
-        if (this.game.gameState.isFinish) return;
-        var scoreEl = document.getElementById(`scoreBoard${this.game.focusGameNumber}`);
-        var lastScoreValues = getLastScoreValues(scoreEl);
-        var lastScoreValue = isFirstPlayer ? lastScoreValues[0] : lastScoreValues[1];
-        var rows = scoreEl.getElementsByClassName('row');
-        var row = isFirstPlayer ? rows[0] : rows[1];
-        row.getElementsByClassName('score')[0].lastChild.value = lastScoreValue + 1;
+      if (this.game.gameState.isFinish) return;
+      var scoreEl = document.getElementById(`scoreBoard${this.game.focusGameNumber}`);
+      var lastScoreValues = getLastScoreValues(scoreEl);
+      var lastScoreValue = isFirstPlayer ? lastScoreValues[0] : lastScoreValues[1];
+      var rows = scoreEl.getElementsByClassName('score-flow')[0].getElementsByClassName('row');
+      var row = isFirstPlayer ? rows[0] : rows[1];
+      row.getElementsByClassName('score')[0].lastChild.value = lastScoreValue + 1;
 
-        this.addScoreDOM(scoreEl);
+      this.addScoreDOM(scoreEl);
+//      this.addMemoDOM(scoreEl);
     },
     deletePoint() {
-        var scoreEl = document.getElementById(`scoreBoard${this.game.focusGameNumber}`);
-        var rows = scoreEl.getElementsByClassName('row');
-        for (var i = 0; i < rows.length; i++) {
-            var score = rows[i].getElementsByClassName('score')[0];
-            if (score.childElementCount <= 1) break;
-            score.removeChild(score.lastChild);
-            score.lastChild.value = "";
-        }
-        this.game.gameState.reset();
+      var scoreEl = document.getElementById(`scoreBoard${this.game.focusGameNumber}`);
+      var rows = scoreEl.getElementsByClassName('score-flow')[0].getElementsByClassName('row');
+      for (var i = 0; i < rows.length; i++) {
+        var score = rows[i].getElementsByClassName('score')[0];
+        if (score.childElementCount <= 1) break;
+        score.removeChild(score.lastChild);
+        score.lastChild.value = '';
+      }
+      this.game.gameState.reset();
     },
     changeFocus(gameNumber) {
-        for (var i = 1; i <= 5; i++) {
-            var scoreEl = document.getElementById(`scoreBoard${i}`);
-            if (i == gameNumber) {
-                scoreEl.classList.add("available");
-                scoreEl.classList.remove("unavailable");
-                this.game.focusGameNumber = gameNumber;
-            } else {
-                scoreEl.classList.remove("available");
-                scoreEl.classList.add("unavailable");
-            }
+      for (var i = 1; i <= 5; i++) {
+        var scoreEl = document.getElementById(`scoreBoard${i}`);
+        if (i == gameNumber) {
+          scoreEl.classList.add('available');
+          scoreEl.classList.remove('unavailable');
+          this.game.focusGameNumber = gameNumber;
+        } else {
+          scoreEl.classList.remove('available');
+          scoreEl.classList.add('unavailable');
         }
+      }
     },
     addScoreDOM(scoreEl) {
-        if (isGameFinish(scoreEl)) {
-            this.game.gameState.finish(scoreEl);
-            this.addResultDOM(scoreEl);
-            return;
-        }
+      if (isGameFinish(scoreEl)) {
+        this.game.gameState.finish(scoreEl);
+        this.addResultDOM(scoreEl);
+        return;
+      }
+      var rows = scoreEl.getElementsByClassName('score-flow')[0].getElementsByClassName('row');
+      for (i = 0; i < rows.length; i++) {
+        var input = document.createElement('input');
+        input.setAttribute('type', 'text');
+        input.setAttribute('onFocus', 'this.blur()');
+        rows[i].getElementsByClassName('score')[0].appendChild(input);
+      };
+    },
+    addMemoDOM(scoreEl) {
+      if (isGameFinish(scoreEl)) return;
+      var memoList = scoreEl.getElementsByClassName('memoList')[0];
+      var memoClasses = memoList.getElementsByClassName('memo');
+      var memo = memoClasses[memoClasses.length - 1];
+      var scoreValues = getLastScoreValues(scoreEl);
+      var scoreValue = `${scoreValues[0]}-${scoreValues[1]}`;
 
-        var rows = scoreEl.getElementsByClassName('row')
-        for (i = 0; i < rows.length; i++) {
-            var input = document.createElement('input');
-            input.setAttribute("type", "text");
-            input.setAttribute("onFocus", "this.blur()");
-            rows[i].getElementsByClassName('score')[0].appendChild(input);
-        };
+      if (!memo || memo.getElementsByTagName('input')[0].value != '') {
+        var row = document.createElement('div');
+        row.classList.add('row');
+
+        var score = document.createElement('div');
+        score.classList.add('col-1', 'score');
+        var pTag = document.createElement('p');
+        pTag.textContent = scoreValue;
+        score.appendChild(pTag);
+
+        var memo = document.createElement('div');
+        memo.classList.add('col-11', 'memo');
+        var input = document.createElement('input');
+        input.setAttribute('type', 'text');
+        input.setAttribute('name', 'memo');
+        memo.appendChild(input);
+
+        row.appendChild(score);
+        row.appendChild(memo);
+
+        memoList.appendChild(row);
+      } else {
+        var scoreClasses = memoList.getElementsByClassName('score');
+        scoreClasses[scoreClasses.length - 1].getElementsByTagName('p')[0].textContent = scoreValue;
+      }
     },
     addResultDOM(scoreEl) {
-        var rows = scoreEl.getElementsByClassName('row')
-        var lastScoreValues = getLastScoreValues(scoreEl);
-        for (i = 0; i < rows.length; i++) {
-            var pTag = document.createElement('p');
-            pTag.classList.add("result");
-            pTag.innerHTML = lastScoreValues[i];
-            rows[i].getElementsByClassName('score')[0].appendChild(pTag);
-        };
+      var rows = scoreEl.getElementsByClassName('score-flow')[0].getElementsByClassName('row');
+      var lastScoreValues = getLastScoreValues(scoreEl);
+      for (i = 0; i < rows.length; i++) {
+        var pTag = document.createElement('p');
+        pTag.classList.add('result');
+        pTag.innerHTML = lastScoreValues[i];
+        rows[i].getElementsByClassName('score')[0].appendChild(pTag);
+      };
     },
     updatePlayer() {
-        this.$set(this, 'player', this.allPlayers.find(p => p.playerID == this.playerID));
-    }
+      this.$set(this, 'player', this.allPlayers.find(p => p.playerID == this.playerID));
+    },
+
   },
   mounted() {
-      document.addEventListener('keydown', e => {
-          if (document.activeElement.tagName == 'INPUT') return
-          // スコア周りの制御
-          if (e.code == `ArrowUp`) {
-              e.preventDefault();
-              this.addPoint(true);
-          } else if (e.code == `ArrowDown`) {
-              e.preventDefault();
-              this.addPoint(false);
-          } else if (e.code == `Backspace`) {
-              e.preventDefault();
-              this.deletePoint();
-          }
-      });
+    document.addEventListener('keydown', e => {
+      if (['INPUT', 'TEXTAREA'].includes(document.activeElement.tagName)) return
+      // スコア周りの制御
+      if (e.code == `ArrowUp`) {
+        e.preventDefault();
+        this.addPoint(true);
+      } else if (e.code == `ArrowDown`) {
+        e.preventDefault();
+        this.addPoint(false);
+      } else if (e.code == `Backspace`) {
+        e.preventDefault();
+        this.deletePoint();
+      }
+    });
   }
 });
 app.initialize();
